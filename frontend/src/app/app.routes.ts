dd535fe4be41d45a6963667db6b4f862ab38de15
Xavier Prat <xavierprat78@gmail.com>
2024-09-25 13:01:12 +0200

    routing funcionant

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { ListproductsComponent } from './components/listproducts/listproducts.component';
 import { AddEditProductComponent } from './components/add-edit-product/add-edit-product.component';
+import { MapaComponent } from './components/mapa/mapa.component';
+import { FullcalendarComponent } from './components/fullcalendar/fullcalendar.component';
+import { GraficsComponent } from './components/grafics/grafics.component';
 
 export const routes: Routes = [
     { path: '', component: ListproductsComponent },
     { path: 'add', component: AddEditProductComponent },
     { path: 'edit/:id', component: AddEditProductComponent },
+    { path: 'mapa', component: MapaComponent },
+    { path: 'fullcalendar', component: FullcalendarComponent },
+    { path: 'grafics', component: GraficsComponent },
     { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,24 +1,26 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http'; // Afegir importació de provideHttpClient
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 import { appConfig } from './app/app.config';
+import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
-    ...appConfig.providers || [],
-    provideHttpClient(), // Afegir provideHttpClient aquí
-    provideAnimations(),
-    provideToastr({
+    provideRouter(routes), // Afegeix el proveïdor de rutes
+    provideHttpClient(),   // Per al client HTTP
+    provideAnimations(),   // Per a les animacions
+    provideToastr({        // Configuració de Toastr
       timeOut: 2000,
       positionClass: 'toast-bottom-right',
       preventDuplicates: true,
-    })
+    }),
+    ...appConfig.providers || []
   ]
 }).catch((err) => console.error(err));
-
 
 
 
